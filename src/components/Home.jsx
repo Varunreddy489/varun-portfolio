18633bc284abd4f6fa96e5a59d617a777a0edbee
Varunreddy <varunsannapureddy@gmail.com>
2024-09-10 13:32:56 +0530

    modified:   package.json 	modified:   public/RESUME.pdf 	modified:   src/components/Home.jsx 	modified:   src/components/Navbar.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,13 @@
 import { FaDownload, FaGithub } from "react-icons/fa";
 
 const Home = () => {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = "../../public/Resume.pdf"; 
+    link.download = "Varunreddy_Resume.pdf";
+    link.click();
+  };
+
   return (
     <div className="flex text-white mt-10 justify-between p-6 ">
       <div>
@@ -15,6 +22,7 @@ const Home = () => {
         <div className="m-5 space-x-5">
           <button
             type="button"
+            onClick={handleDownload}
             className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 text-gray-500 hover:border-blue-600 hover:text-blue-600 hover:scale-110 ease-in focus:outline-none focus:border-blue-600 focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:border-neutral-700 dark:text-neutral-400 dark:hover:text-blue-500 dark:hover:border-blue-600 dark:focus:text-blue-500 dark:focus:border-blue-600 transition-transform duration-300"
           >
             <FaDownload />
